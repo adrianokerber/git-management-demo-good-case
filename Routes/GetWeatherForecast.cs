7f49ae56730256dc349031de441155c5f8a50410
Adriano Kerber <kerberpro@gmail.com>
2024-03-21 13:29:54 -0300

    ⚰️ [GMD-0007] - Remove código comentado

--- Routes/GetWeatherForecast.cs
+++ Routes/GetWeatherForecast.cs
@@ -2,31 +2,6 @@ namespace projeto_ruim.Routes;
 
 public class GetWeatherForecast : IHttpRoute
 {
-    // public WebApplication RegisterEndpointGetWeatherForecast(WebApplication app)
-    // {
-    //     var summaries = new[]
-    //     {
-    //         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    //     };
-    //     
-    //     app.MapGet("/weatherforecast", () =>
-    //         {
-    //             var forecast =  Enumerable.Range(1, 5).Select(index =>
-    //                     new WeatherForecast
-    //                     (
-    //                         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-    //                         Random.Shared.Next(-20, 55),
-    //                         summaries[Random.Shared.Next(summaries.Length)]
-    //                     ))
-    //                 .ToArray();
-    //             return forecast;
-    //         })
-    //         .WithName("GetWeatherForecast")
-    //         .WithOpenApi();
-    //
-    //     return app;
-    // }
-    
     private static readonly string[] _summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
